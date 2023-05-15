53232db9939ff593c77e575371ef65a3b7c52959
amsuyang <93066479+amsuyang@users.noreply.github.com>
2023-05-15 18:18:03 +0800

    add utils:merge_blank_lines.rb

---
+++ utils/merge_blank_lines.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require 'find'
+
+data_dir = File.expand_path('~/repos/safedata/english/nce-02')
+
+# merge consecutive blank lines
+def merge_blank_lines(lines)
+  new_lines = []
+  previous_line_is_blank = false
+  current_line_is_blank = false
+  regex_blank_line = /^$/
+
+  lines.each do |line|
+    current_line_is_blank = if regex_blank_line =~ line
+                              true
+                            else
+                              false
+                            end
+
+    next if previous_line_is_blank && current_line_is_blank
+
+    previous_line_is_blank = current_line_is_blank
+
+    new_lines.push(line)
+  end
+
+  new_lines
+end
+
+Find.find(data_dir) do |name|
+  next if ['.', '..'].include?(name)
+  next if File.directory? name
+
+  file = File.open(name, 'r')
+
+  lines = merge_blank_lines(file.readlines)
+
+  file.close
+
+  file = File.open(name, 'w')
+
+  lines.each do |line|
+    line.chomp!
+    file.puts line
+  end
+end
