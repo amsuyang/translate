b346a706fd4c4a6201a2493f42adef03d29748d1
amsuyang <93066479+amsuyang@users.noreply.github.com>
2023-05-16 04:13:12 +0800

    add check_paragraph_structure.rb

---
+++ utils/check_paragraph_structure.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'find'
+
+require_relative 'data_dir'
+
+Find.find($data_dir) do |name|
+  next if ['.', '..'].include?(name)
+  next if File.directory? name
+
+  file = File.open(name, 'r')
+
+  lines = file.readlines
+
+  file.close
+
+  blank_lines_count = 0
+  lines.each do |line|
+    if /^$/ =~ line
+      blank_lines_count += 1
+    end
+  end
+
+  puts "#{name}, '==>', #{blank_lines_count}" if blank_lines_count != 6
+end
