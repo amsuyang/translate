01a97319b85bfc205a71c7492d413f2886ca1e5e
amsuyang <93066479+amsuyang@users.noreply.github.com>
2023-05-16 04:12:53 +0800

    add normalize_blank_line.rb

--- utils/README.md
+++ utils/README.md
@@ -1,4 +1,5 @@
 nce-02:
-1,merge_blank_lines.rb
-2,delete_top_blank_line.rb
-3,check_paragraph_structure.rb
+1,normalize_blank_line.rb
+2,merge_blank_lines.rb
+3,delete_top_blank_line.rb
+4,check_paragraph_structure.rb
---
+++ utils/normalize_blank_line.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+# delete white space characters (spaces, tabs, etc.) from blank lines
+
+require 'find'
+
+require_relative 'data_dir'
+
+Find.find($data_dir) do |name|
+  next if ['.', '..'].include?(name)
+  next if File.directory? name
+
+  new_lines = []
+
+  File.open(name) do |file|
+    file.each_line do |line|
+      if /^\s+$/ =~ line
+        new_lines.push "\n"
+      else
+        line.chomp!
+        new_lines.push line
+      end
+    end
+  end
+
+  File.open(name, 'w') do |file|
+    new_lines.each do |line|
+      file.puts line
+    end
+  end
+end
