8f0cf94fdb9cdd73515093d4aa20194109ad94e3
amsuyang <93066479+amsuyang@users.noreply.github.com>
2023-05-15 19:24:53 +0800

    add delete_top_blank_line.rb

---
+++ utils/delete_top_blank_line.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'find'
+
+require_relative 'data_dir'
+
+Find.find($data_dir) do |name|
+  next if ['.', '..'].include?(name)
+  next if File.directory? name
+
+  file = File.open(name, 'r')
+
+  lines = file.readlines
+
+  file.close
+
+  if /^$/ =~ lines[0]
+    lines.shift
+  end
+
+  file = File.open(name, 'w')
+
+  lines.each do |line|
+    line.chomp!
+    file.puts line
+  end
+
+  file.close
+end
